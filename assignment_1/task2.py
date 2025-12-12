70bee3ceab0c2c4a0f0c209ed4970c261d600e82
eqzce <97641078+eqzce@users.noreply.github.com>
2025-12-12 14:11:32 +0500

    Update task2.py

--- assignment_1/task2.py
+++ assignment_1/task2.py
@@ -2,3 +2,10 @@ s1, s2, s3 = input("Enter salaries of 3 employees: ").split()
 maxsal = max(int(s1),int(s2),int(s3))
 minsal = min(int(s1),int(s2),int(s3))
 print(maxsal-minsal)
+
+# or
+
+s1, s2, s3 = map(int, input("Enter salaries of 3 employees: ").split())
+maxsal = max(s1,s2,s3)
+minsal = min(s1,s2,s3)
+print(maxsal-minsal)
