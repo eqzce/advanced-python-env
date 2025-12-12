393b8eab79f98029ab836ca4d56b1fd081c84d7d
eqzce <97641078+eqzce@users.noreply.github.com>
2025-12-12 14:12:30 +0500

    Create task5.py

---
+++ assignment_1/task5.py
@@ -0,0 +1,5 @@
+prod = int(input("Enter guessed num multiplied by 5: "))
+add = int(input("Add 8 for the result: "))
+prod2 = int(input("Multuply by 2: "))
+ans = (prod2/2-8)/5
+print("Your num is: ", ans)
