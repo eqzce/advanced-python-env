b70691dbfd70449b4cdf6089c962d1469238cc7d
eqzce <97641078+eqzce@users.noreply.github.com>
2025-12-23 16:08:34 +0500

    Update task2.py

--- assignment_2/task2.py
+++ assignment_2/task2.py
@@ -1,18 +1,8 @@
-text = input("Type string: ")
-pattern1 = ">>-->"
-pattern2 = "<--<<"
-count1 = text.count(pattern1)
-count2 = text.count(pattern2)
-ans = count1 + count2
-print(ans)
-
-# or
-
-import re
-text = input("Type string: ")
-pattern1 = ">>-->"
-pattern2 = "<--<<"
-count1 = len(re.findall(f"(?={pattern1})", text))
-count2 = len(re.findall(f"(?={pattern2})", text))
-ans = count1+count2
-print(ans)
+str = input()
+cycle = input()
+n = len(cycle)
+count = 0
+for i in range(len(str) - n + 1):
+    if str[i:i+n] in cycle + cycle:
+        count+=1
+print(count)
