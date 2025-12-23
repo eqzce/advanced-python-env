9621e51852e64a0763ff574f4a61f9a8148d5db5
eqzce <97641078+eqzce@users.noreply.github.com>
2025-12-23 16:08:51 +0500

    Update task1.py

--- assignment_2/task1.py
+++ assignment_2/task1.py
@@ -5,3 +5,14 @@ count1 = text.count(pattern1)
 count2 = text.count(pattern2)
 ans = count1 + count2
 print(ans)
+
+# or
+
+import re
+text = input("Type string: ")
+pattern1 = ">>-->"
+pattern2 = "<--<<"
+count1 = len(re.findall(f"(?={pattern1})", text))
+count2 = len(re.findall(f"(?={pattern2})", text))
+ans = count1+count2
+print(ans)
