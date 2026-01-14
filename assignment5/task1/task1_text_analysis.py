224cfeb948d067f223dda94849ad8e392abf415d
eqzce <97641078+eqzce@users.noreply.github.com>
2026-01-14 14:19:15 +0500

    Update task1_text_analysis.py

--- assignment5/task1/task1_text_analysis.py
+++ assignment5/task1/task1_text_analysis.py
@@ -1,23 +1,20 @@
 import string
 
 def analyze_text(input_file, output_file):
-    word_freq = {}
-    line_count = 0
-    word_count = 0
-
     with open(input_file, encoding="utf-8") as file:
-        for line in file:
-            line_count += 1
+        lines = file.readlines()
 
-            line = line.translate(
-                str.maketrans("", "", string.punctuation)
-            ).lower()
+    line_count = len(lines)
+    word_count = 0
+    word_freq = {}
 
-            words = line.split()
-            word_count += len(words)
+    for line in lines:
+        line = line.translate(str.maketrans("", "", string.punctuation)).lower()
+        words = line.split()
+        word_count += len(words)
 
-            for word in words:
-                word_freq[word] = word_freq.get(word, 0) + 1
+        for word in words:
+            word_freq[word] = word_freq.get(word, 0) + 1
 
     with open(output_file, "w", encoding="utf-8") as file:
         file.write(f"Total lines: {line_count}\n")
@@ -26,6 +23,7 @@ def analyze_text(input_file, output_file):
 
         for word, count in word_freq.items():
             file.write(f"{word}: {count}\n")
+
 
 
 if __name__ == "__main__":
